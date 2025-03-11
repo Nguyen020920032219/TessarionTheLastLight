8e7923a174431d69a72e5103e5eab6e980199a16
Nguyên Trần <trandinhnguyen46159@gmail.com>
2025-03-12 00:15:25 +0700

    setup player weapon

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,39 +6,52 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce = 15f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform groundCheck;
+
     private Animator animator;
-    private bool isGrounded;
     private Rigidbody2D rigidbody;
+    private bool isGrounded;
+    private WeaponManager weaponManager;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
+        weaponManager = GetComponent<WeaponManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         HandleMovement();
         HandleJump();
         UpdateAnimation();
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            weaponManager.SwitchWeapon();
+        }
+
+        if (Input.GetKey(KeyCode.G))
+        {
+            weaponManager.Attack();
+        }
+        else if (Input.GetKeyUp(KeyCode.G))
+        {
+            weaponManager.StopAttack();
+        }
     }
 
     private void HandleMovement()
     {
         float moveInput = Input.GetAxis("Horizontal");
-        if (animator.GetBool("isAttacking"))
+
+        if (animator.GetBool("isAttacking") || animator.GetBool("isShooting"))
         {
             rigidbody.linearVelocity = new Vector2(0, rigidbody.linearVelocity.y);
             return;
         }
+
         rigidbody.linearVelocity = new Vector2(moveInput * moveSpeed, rigidbody.linearVelocity.y);
+
         if (moveInput > 0) transform.localScale = new Vector3(1, 1, 1);
         else if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
     }
@@ -57,8 +70,7 @@ public class PlayerController : MonoBehaviour
     {
         bool isRunning = Mathf.Abs(rigidbody.linearVelocity.x) > 0.1f;
         bool isJumping = !isGrounded;
-        bool isAttacking = Input.GetKey(KeyCode.G);
-        animator.SetBool("isAttacking", isAttacking);
+
         animator.SetBool("isRunning", isRunning);
         animator.SetBool("isJumping", isJumping);
     }
