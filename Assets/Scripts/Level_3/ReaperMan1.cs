4f5a3e54f2c09488c31f5a4c48e3e8452493676d
Vo Thanh Phat <vophat54321@gmail.com>
2025-03-23 14:34:01 +0700

    Add enemy to scene level_3

---
+++ Assets/Scripts/Level_3/ReaperMan1.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class ReaperMan1 : MonoBehaviour
+{
+    [SerializeField] private float maxHP = 50f;
+    [SerializeField] private float moveSpeed = 1f;
+    private GameObject player;
+    private float currentHP;
+    private Animator animator;
+    private bool isDead = false;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
+        currentHP = maxHP;
+    }
+
+    private void Update()
+    {
+        if (isDead || player == null) return;
+
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        transform.position += direction * moveSpeed * Time.deltaTime;
+
+        if (direction.x > 0) transform.localScale = new Vector3(0.03f, 0.03f, 0.03f);
+        else if (direction.x < 0) transform.localScale = new Vector3(-0.03f, 0.03f, 0.03f);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (isDead) return;
+        currentHP -= damage;
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+        animator.SetBool("isDying", true);
+        Destroy(gameObject, 1.5f);
+    }
+}
