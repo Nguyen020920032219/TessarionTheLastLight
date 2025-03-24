1631856dc0e718cb7ff0fb8a97c27493a48aa90f
Vo Thanh Phat <vophat54321@gmail.com>
2025-03-24 10:43:58 +0700

    change code of enemy level_3

---
+++ Assets/Scripts/Enemy/ReaperMan1.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReaperMan1 : Enemy
+{
+    [SerializeField] private Image hpBar;
+    [SerializeField] private float hp = 50f;
+    [SerializeField] private float damage = 5f;
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float objScale = 0.03f;
+    void Start()
+    {
+        InitStat(hp, damage, speed, objScale, hpBar);
+        UpdateHpBar();
+    }
+    void Update()
+    {
+        MoveToPlayer();
+    }
+}
