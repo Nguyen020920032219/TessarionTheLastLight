9859acc342ad57f4f11e8da4ae7c3845bdeafab8
Nguyên Trần <trandinhnguyen46159@gmail.com>
2025-03-29 05:37:27 +0700

    modify scene 2

--- Assets/Scripts/Stone/ChangeScene1.cs
+++ Assets/Scripts/Stone/ChangeScene1.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour
+public class ChangeScene1 : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
