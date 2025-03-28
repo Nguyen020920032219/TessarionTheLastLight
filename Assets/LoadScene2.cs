7f8fed3b67fbbfeeef0b1a518e732ad79b1a1376
Nguyên Trần <trandinhnguyen46159@gmail.com>
2025-03-29 04:53:08 +0700

    modify change scene script

---
+++ Assets/LoadScene2.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene2 : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
--- Assets/Scripts/LoadScene1.cs
+++ Assets/Scripts/LoadScene1.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadScene : MonoBehaviour
+public class LoadScene1 : MonoBehaviour
 {
     void Start()
     {
